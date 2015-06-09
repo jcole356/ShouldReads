80638c3b29a4c89d28ed23d9804ceb09454f5b7e
Joshua Cole <j_cole_356@yahoo.com>
2015-06-09 15:39:39 -0700

    implement basic auth with sign up and log in pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def auth_token_input
+    "<input
+        type=\"hidden\"
+        name=\"authenticity_token\"
+        value=\"#{ form_authenticity_token }\">".html_safe
+  end
+
+  def flash_errors
+    flash[:errors].join("<br>").html_safe if flash[:errors]
+  end
 end
