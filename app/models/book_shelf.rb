a88ba23ea1f820092304b454da9151c55608140a
Joshua Cole <j_cole_356@yahoo.com>
2015-06-15 09:36:08 -0700

    add uniqueness validations for bookshelves and reviews

--- app/models/book_shelf.rb
+++ app/models/book_shelf.rb
@@ -1,5 +1,7 @@
 class BookShelf < ActiveRecord::Base
   validates :title, :owner_id, presence: true
+  validates :title, length: { maximum: 20 }
+  validates :title, uniqueness: { scope: :owner_id }
 
   belongs_to(
     :user,
@@ -11,5 +13,5 @@ class BookShelf < ActiveRecord::Base
   has_many :book_shelvings, foreign_key: :shelf_id
 
   has_many :books, through: :book_shelvings, source: :book
-  
+
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,6 @@
 class Review < ActiveRecord::Base
   validates :author_id, :book_id, :rating, :title, presence: true
+  validates :author_id, uniqueness: { scope: :book_id }
 
   belongs_to :user, foreign_key: :author_id
   belongs_to :book
