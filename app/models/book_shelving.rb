ab698141b9ad6d0db43cd2c7ed32de3108e3241c
Joshua Cole <j_cole_356@yahoo.com>
2015-06-15 17:19:21 -0700

    add dependent destroy on book_shelving book relationship

--- app/models/book_shelving.rb
+++ app/models/book_shelving.rb
@@ -1,4 +1,4 @@
 class BookShelving < ActiveRecord::Base
   belongs_to :book
-  belongs_to :book_shelf, foreign_key: :shelf_id
+  belongs_to :book_shelf, foreign_key: :shelf_id, dependent: :destroy
 end
