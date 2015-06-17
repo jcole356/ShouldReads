0bb30b689619023c4bfd799e637d8c7dfd602613
Joshua Cole <j_cole_356@yahoo.com>
2015-06-17 11:06:52 -0700

    pass data for search book to the template

--- app/assets/javascripts/models/search_book.js
+++ app/assets/javascripts/models/search_book.js
@@ -1,23 +1,8 @@
 ShouldReads.Models.SearchBook = Backbone.Model.extend({
-  // This needs to be set up as a giant conditional to accommodate
-  // results with no image
-
   initialize: function() {
   },
 
-  parseAuthor: function(){
-
-  },
-
-  parseImage: function(){
-
-  },
-
-  // I think I may need to actually write a unique conditional for
-  // each of these because they are nested differently.
   parse: function(response) {
-      // var attrs = ['title', 'authors', 'imageLinks', 'pageCount', 'description'];
-      // var attrs = [title, authors, imageLinks, pageCount, description];
     if (response.volumeInfo) {
         if (response.volumeInfo.authors) {
           var author = response.volumeInfo.authors[0];
--- app/assets/javascripts/views/search/search_books.js
+++ app/assets/javascripts/views/search/search_books.js
@@ -3,15 +3,24 @@ ShouldReads.Views.SearchIndex = Backbone.View.extend({
 
   className: "book-index",
 
-  initialize: function () {
+  events: {
+    "click .index-book-info": "addOrRetrieve"
+  },
+
+  initialize: function() {
     this.listenTo(this.collection, "add", this.render);
   },
 
-  render: function () {
+  render: function() {
     var content = this.template({
       searchBooks: this.collection
     });
     this.$el.html(content);
     return this;
+  },
+
+  addOrRetrieve: function(event) {
+    var target = $(event.currentTarget);
+    debugger;
   }
 });
