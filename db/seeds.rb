e3f57b1751596e93c595acabb1ae67a0433f3b9d
Joshua Cole <j_cole_356@yahoo.com>
2015-06-18 16:50:35 -0700

    add guest login and new seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-# u1 = User.create(username: "reader", password: "foobar")
+u1 = User.create(username: "kurt", password: "vonnegut")
 
 # b1 = Book.create(title: "Harry Potter and the Half Blood Prince",
 #                  author: "JK Rowling",
