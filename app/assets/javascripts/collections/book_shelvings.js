249170ca6290cbe48a9ac3bf13f9321f7da3e44a
Joshua Cole <j_cole_356@yahoo.com>
2015-08-24 19:37:25 -0700

    add comparator to the bookshelving collection to display most recent first

--- app/assets/javascripts/collections/book_shelvings.js
+++ app/assets/javascripts/collections/book_shelvings.js
@@ -1,5 +1,16 @@
 ShouldReads.Collections.BookShelvings = Backbone.Collection.extend({
   url: "/api/book_shelvings",
 
-  model: ShouldReads.Models.BookShelving
+  model: ShouldReads.Models.BookShelving,
+
+  // Not sure if this is really doing anything or not.  The All shelf is
+  // working the way I would like it to, but not the others.
+  // The others aren't working correctly because I haven't refactored
+  // the views yet.
+  comparator: function(review) {
+    debugger
+    var dateString = review.get('created_at');
+    var date = new Date(dateString);
+    return -date.getTime();
+  }
 });
