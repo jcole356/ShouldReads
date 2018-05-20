2f8cef841bf672f8b55108dd0c467b44997dc035
Joshua Cole <j_cole_356@yahoo.com>
2018-05-19 23:55:39 -0700

    add parse function to user model

--- app/assets/javascripts/models/user.js
+++ app/assets/javascripts/models/user.js
@@ -1,3 +1,13 @@
 ShouldReads.Models.User = Backbone.Model.extend({
-  urlRoot: "users"
+  urlRoot: "users",
+
+  parse: function(user) {
+    this.set({
+      id: user.id,
+      loginCount: user.login_count_b,
+      username: user.username,
+    });
+
+    return this;
+  }
 });
