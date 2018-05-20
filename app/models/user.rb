d983655bfc1d1aa8525ac9b4d8e370e6081cbf47
Joshua Cole <j_cole_356@yahoo.com>
2018-05-19 23:51:54 -0700

    remove login count increment code

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ class User < ActiveRecord::Base
     class_name: "BookShelf",
     foreign_key: :owner_id,
     primary_key: :id
-    )
+  )
 
   has_many :reviews, foreign_key: :author_id
   has_many :book_shelvings, through: :book_shelves, source: :book_shelvings
