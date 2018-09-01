b365b5491651f80d0bb0be43afd6de8786db6e39
Joshua Cole <joshuacole356@gmail.com>
2018-09-01 13:22:22 -0700

    make search results mobile friendly

--- app/assets/javascripts/views/search/searchBooks.js
+++ app/assets/javascripts/views/search/searchBooks.js
@@ -1,26 +1,26 @@
 ShouldReads.Views.SearchIndex = Backbone.CompositeView.extend({
   template: JST['search/results'],
 
-  className: "search-index",
+  className: "search-index row",
 
-  initialize: function() {
+  initialize: function () {
     this.listenTo(this.collection, "add", this.addResult);
   },
 
-  render: function() {
+  addResult: function (book) {
+    var view = new ShouldReads.Views.SearchBook({
+      model: book
+    });
+
+    this.addSubview('.search-book', view);
+  },
+
+  render: function () {
     var content = this.template({
       searchBooks: this.collection
     });
     this.$el.html(content);
 
     return this;
-  },
-
-  addResult: function(book) {
-    var view = new ShouldReads.Views.SearchBook({
-      model: book
-    });
-
-    this.addSubview('.search-book', view);
   }
 });
