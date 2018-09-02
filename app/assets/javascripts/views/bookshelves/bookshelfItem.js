96dff64ee3a2d731cc1d0218999557a901b679a1
Joshua Cole <joshuacole356@gmail.com>
2018-09-02 11:34:51 -0700

    use rateYo plugin to display ratings in shelf books

--- app/assets/javascripts/views/bookshelves/bookshelfItem.js
+++ app/assets/javascripts/views/bookshelves/bookshelfItem.js
@@ -10,12 +10,32 @@ ShouldReads.Views.ShelfItem = Backbone.View.extend({
     this.listenTo(this.model, 'add', this.render);
   },
 
+  initializeRateYo: function () {
+    var $rateYo = this.$('.rateYo');
+    if (!$rateYo.length) {
+      return;
+    }
+    var rating;
+    if (this.all) {
+      rating = this.model._book.get('average_rating');
+    } else {
+      rating = this.model.get('average_rating');
+    }
+    $rateYo.rateYo({
+      fullStar: true,
+      rating: rating,
+      readOnly: true,
+      starWidth: '15px',
+    });
+  },
+
   render: function () {
     var content = this.template({
       book: this.all ? this.model._book : this.model,
       title: this.all ? this.model.get('title') : false,
     });
     this.$el.html(content);
+    this.initializeRateYo();
     
     return this;
   },
