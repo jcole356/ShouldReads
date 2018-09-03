20f984464b2cecd0a982ab0fc797c8b99610cfc0
Joshua Cole <joshuacole356@gmail.com>
2018-09-02 23:29:32 -0700

    use rateYo for book review ratings

--- app/assets/javascripts/views/books/bookReviews.js
+++ app/assets/javascripts/views/books/bookReviews.js
@@ -3,16 +3,34 @@ ShouldReads.Views.BookReviews = Backbone.View.extend({
 
   className: "book-review-content",
 
-  initialize: function() {
+  initialize: function () {
     this.listenTo(this.collection, 'add sync remove', this.render);
   },
 
-  render: function() {
+  initializeRateYos: function () {
+    var $rateYos = this.$('.rateYo');
+    if (!$rateYos.length) {
+      return;
+    }
+    $rateYos.each(function (idx, rateYo) {
+      $rateYo = $(rateYo);
+      rating = $rateYo.data('rating');
+      $rateYo.rateYo({
+        fullStar: true,
+        rating: rating,
+        readOnly: true,
+        starWidth: '25px',
+      });
+    });
+  },
+
+  render: function () {
     var content = this.template({
       book: this.model,
       reviews: this.collection,
     });
     this.$el.html(content);
+    this.initializeRateYos();
 
     return this;
   },
