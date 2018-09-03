53d3f16ca19f1c3d025aaf06b5866610da82b491
Joshua Cole <joshuacole356@gmail.com>
2018-09-02 17:15:21 -0700

    Add RateYo to newsfeed and book show (#20)
    * use rateYo for newsfeed ratings
    
    * use rateYo for book show ratings

--- app/assets/javascripts/views/newsfeed/item.js
+++ app/assets/javascripts/views/newsfeed/item.js
@@ -3,11 +3,25 @@ ShouldReads.Views.FeedItem = Backbone.View.extend({
 
   className: "feed-item row",
 
+  initializeRateYo: function () {
+    var $rateYo = this.$('.rateYo');
+    if (!$rateYo.length) {
+      return;
+    }
+    $rateYo.rateYo({
+      fullStar: true,
+      rating: this.model.get('rating'),
+      readOnly: true,
+      starWidth: '25px',
+    });
+  },
+
   render: function () {
     var content = this.template({
       review: this.model
     });
     this.$el.html(content);
+    this.initializeRateYo();
 
     return this;
   },
