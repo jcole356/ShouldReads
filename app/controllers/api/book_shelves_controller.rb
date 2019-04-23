eedf20383abeab1e04385154992039436c2f43d9
Joshua Cole <joshuacole356@gmail.com>
2019-04-22 23:18:38 -0700

    Fix review queries (#26)
    * removes debugger and unnecessary variable assignment
    * eager loads reviews for average rating
    * eager loads reviews for book_shelving controller

--- app/controllers/api/book_shelves_controller.rb
+++ app/controllers/api/book_shelves_controller.rb
@@ -11,7 +11,7 @@ class Api::BookShelvesController < ApplicationController
   end
 
   def index
-    @book_shelves = current_user.book_shelves.includes(:book_shelvings => [:book])
+    @book_shelves = current_user.book_shelves.includes(book_shelvings: [book: [:reviews]])
     render :index
   end
 
--- app/views/api/book_shelvings/index.json.jbuilder
+++ app/views/api/book_shelvings/index.json.jbuilder
@@ -1,5 +1,3 @@
-# Eager load books and bookshelves
-@book_shelvings = @current_user.book_shelvings.includes(:book, :book_shelf)
 json.array! @book_shelvings do |book_shelving|
   json.extract! book_shelving, :id, :shelf_id, :book_id, :created_at
   json.extract! book_shelving.book_shelf, :title
